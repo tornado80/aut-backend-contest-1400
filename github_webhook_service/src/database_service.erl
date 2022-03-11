e277625a62094df3bd7465d24f1e5a719af5e334
Amirhosein Rajabi <a.h.rajabi@outlook.com>
2022-03-11 09:55:53 +0330

    Fix error in database service

--- github_webhook_service/src/database_service.erl
+++ github_webhook_service/src/database_service.erl
@@ -8,7 +8,7 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, insert_submission/2]).
+-export([start_link/0, insert_submission/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
     code_change/3]).
 
