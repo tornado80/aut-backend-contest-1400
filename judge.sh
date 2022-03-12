e217fd53a874c6a07f24296570da2a1174921b8b
Amirhosein Rajabi <a.h.rajabi@outlook.com>
2022-03-12 11:28:54 +0000

    Fix judge.sh

--- judge.sh
+++ judge.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(conda run -n judge-environment --no-capture-output --live-stream python main.py $1 $2 $3 $4 $5) &> /home/amirhosein/judge/$1.log
+(conda run -n judge-environment --no-capture-output --live-stream python main.py "$1" "$2" "$3" "$4" "$5") &> /home/amirhosein/judge/"$2".log
